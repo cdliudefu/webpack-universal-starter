e3a7886d717633d4bd732688fdaf039c445c19e4
cdliudefu <cdliudefu@126.com>
2020-03-24 17:33:43 +0800

    打包生产环境

---
+++ webpack/webpack.prd.js
@@ -0,0 +1,68 @@
+/**
+ *生产环境
+ **/
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const merge = require('webpack-merge')
+const config = require('./webpack.config')
+
+const devConfig = {
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    filename: '[name]_[hash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, '../dist')
+    }),
+    new OptimizeCssAssetsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash].css'
+    }),
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true
+    })
+  ]
+}
+
+module.exports = merge(config, devConfig)
